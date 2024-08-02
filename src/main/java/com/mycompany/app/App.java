d1e3c0d064d880e4dbe8e740920350b1d77d4dbc
gopi <gopikrishnadasari49@gmail.com>
2024-08-02 10:51:39 +0530

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -17,3 +17,22 @@ public class App {
         return MESSAGE;
     }
 }
+package com.mycompany.app;
+
+/**
+ * Hello world!
+ */
+public class App {
+
+    private static final String MESSAGE = "Hello World!";
+
+    public App() {}
+
+    public static void main(String[] args) {
+        System.out.println(MESSAGE);
+    }
+
+    public String getMessage() {
+        return MESSAGE;
+    }
+}
